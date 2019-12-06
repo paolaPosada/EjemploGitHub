66c536690a5fa508c85556eaf32e71837e972586
Paola <paola.posada1@udea.edu.co>
2019-12-06 17:35:56 -0500

    Commit desde IntelliJ 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hola mundo");
-        System.out.println("Actualizar desde IntelliJ IDEA");
+        System.out.println("Actualizar desde IntelliJ IDEA, intento 2");
     }
 }
